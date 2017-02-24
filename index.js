426e63bed2151399445674a480ac22ede7ec8264
fov42550564 <42550564@qq.com>
2017-02-24 17:03:00 +0800

    使用Animated.View来显示Card

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ var {
     StyleSheet,
     View,
     ScrollView,
+    Animated,
 } = ReactNative;
 
 var Card = React.createClass({
+    getInitialState() {
+        const {height, width, top, marginLeft} = this.props;
+        return {
+            _height: new Animated.Value(height),
+            _width: new Animated.Value(width),
+            _top: new Animated.Value(top),
+            _marginLeft: new Animated.Value(marginLeft)
+        };
+    },
+    componentWillReceiveProps(nextProps) {
+        const {_width, _height, _top, _marginLeft} = this.state;
+        _width.setValue(nextProps.width);
+        _height.setValue(nextProps.height);
+        _top.setValue(nextProps.top);
+        _marginLeft.setValue(nextProps.marginLeft);
+    },
     render() {
-        const {width, height, top, marginLeft, mounted, renderRow} = this.props;
+        const {mounted, renderRow} = this.props;
+        const {_width: width, _height: height, _top: top, _marginLeft: marginLeft} = this.state;
         return (
             mounted ?
-            <View style={[styles.card, {height, width, top, marginLeft}]}>
-                {renderRow(width, height)}
-            </View>
+            <Animated.View style={[styles.card, {height, width, top, marginLeft}]}>
+                {renderRow(this.props.width, this.props.height)}
+            </Animated.View>
             : null
         );
     }
